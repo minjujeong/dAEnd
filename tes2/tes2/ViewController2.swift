39f0bbc6bb8f3b9e0718369385376315f03ff756
minjujeong <rianbow369@naver.com>
2017-02-07 18:05:10 +0900

    filter
    filter

--- tes2/tes2/ViewController2.swift
+++ tes2/tes2/ViewController2.swift
@@ -37,21 +37,33 @@ class ViewController2: UIViewController {
         
         guard let image = self.imageView.image?.cgImage else { return }
         //이게 뭐냐면 가드렛안되면 그냥 브레이크하는것임
+        // nil 이 아니면 다음 문단으로 넘어가라
+        // ui 가 아니라 cgimage로 불러왔다
         
         let openGLContext = EAGLContext(api: .openGLES3)
+        // open geo context // gpu에 접근하는 것. 필터 매우 복잡한 작업이기 때문에
         let context = CIContext(eaglContext: openGLContext!)
+        // open image context // core image와 gpu를 잇는 conjunction
         
         let ciImage = CIImage(cgImage: image)
-        
+        // create a core image
         let filter = CIFilter(name: "CISepiaTone")
-        filter?.setValue(ciImage, forKey: kCIInputImageKey)
-        filter?.setValue(1 ,forKey: kCIInputIntensityKey)
+        // create a filter : we want to use
         
+        filter?.setValue(ciImage, forKey: kCIInputImageKey) // 필터가 적용될 이미지로 ciimage를 넣어라
+        filter?.setValue(1, forKey: kCIInputIntensityKey) // 필터의 적용 강도를 1로 정해라
+        
+        
+       /* if let sepiaOutput = filter?.value(forKey: kCIOutputImageKey) as? CIImage {
+            let exposureFilter = CIFilter(name: "CIExposureAdjust")
+            exposureFilter?.setValue(sepiaOutput, forKey: kCIInputImageKey)
+            exposureFilter?.setValue(1, forKey: kCIInputEVKey) */
+            
         if let output = filter?.value(forKey: kCIOutputImageKey) as? CIImage{
             self.imageView?.image = UIImage(cgImage: context.createCGImage(output, from: output.extent)!)
         }
         
-        
+        // kCIOutputImageKey 필터를 적용해라 - 그것을 ciimage로 타입 변환해라 - 그리고 다시 uiimage로 넣어줘라 : context에 우리의 아웃풋을 넣자 = 이것은 이미지뷰로 넣어질 것
         
         
     }
