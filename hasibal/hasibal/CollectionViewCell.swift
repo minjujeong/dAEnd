db401015e251b688e9ed451a7525fefa64515588
junhyungcha <cscoi045@cscoi045ui-iMac.local>
2017-02-07 17:41:21 +0900

    collectionView
    collectionView

---
+++ hasibal/hasibal/CollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CollectionViewCell.swift
+//  hasibal
+//
+//  Created by cscoi045 on 2017. 2. 7..
+//  Copyright © 2017년 test. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imgVu: UIImageView!
+
+}
---
+++ hasibal/hasibal/ViewPhoto.swift
@@ -0,0 +1,46 @@
+//
+//  ViewPhoto.swift
+//  hasibal
+//
+//  Created by cscoi045 on 2017. 2. 7..
+//  Copyright © 2017년 test. All rights reserved.
+//
+
+import UIKit
+import Photos
+
+class ViewPhoto: UIViewController {
+
+    var assetCollection : PHAssetCollection!
+    var photosAsset : PHFetchResult<PHAsset>!
+    var index : Int = 0
+
+    
+    @IBOutlet weak var bigPhoto: UIImageView!
+    @IBOutlet weak var cancel: UIBarButtonItem!
+    @IBOutlet weak var share: UIBarButtonItem!
+    @IBOutlet weak var trash: UIBarButtonItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
